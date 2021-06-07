c08b410def33c56e036f13ef448387cd8d399166
argas7 <argas@cin.ufpe.br>
2021-06-07 20:57:43 -0300

    Feature(create-controllers): adjust routes to import post controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const keystone = require('keystone');
 const cors = require('cors');
 
-const Post = keystone.list('Posts');
+const postController = require('../controllers/postController');
 const AboutUs = keystone.list('AboutUs');
 
 module.exports = (app) => {
@@ -12,15 +12,7 @@ module.exports = (app) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
-  app.get('/api/posts', (req, res) => {
-    Post.model.find((err, data) => {
-      if (err) {
-        res.status(500).send('DB Error');
-      } else {
-        res.send(data);
-      }
-    });
-  });
+  app.get('/api/posts', postController);
 
   app.get('/api/about-us', (req, res) => {
     AboutUs.model.find((err, data) => {
