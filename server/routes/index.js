c9b1be21f0db09b043550c7c7a5d55c3fc446596
argas7 <argas@cin.ufpe.br>
2021-06-07 21:02:00 -0300

    Feature(create-controllers): adjust routes to import aboutus controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,8 @@
 const path = require('path');
-const keystone = require('keystone');
 const cors = require('cors');
 
 const postController = require('../controllers/postController');
-const AboutUs = keystone.list('AboutUs');
+const aboutUsController = require('../controllers/aboutUsController');
 
 module.exports = (app) => {
   app.use(cors());
@@ -13,16 +12,7 @@ module.exports = (app) => {
   });
 
   app.get('/api/posts', postController);
-
-  app.get('/api/about-us', (req, res) => {
-    AboutUs.model.find((err, data) => {
-      if (err) {
-        res.status(500).send('DB Error');
-      } else {
-        res.send(data);
-      }
-    })
-  });
+  app.get('/api/about-us', aboutUsController);
 
   app.get('*', (req, res) => {
 		res.redirect('/');
